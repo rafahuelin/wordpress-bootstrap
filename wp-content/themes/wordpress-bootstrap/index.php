bb7d3a07e72450de1600ac0dbd39724fd685f723
Rafa Huelin <it@engisa.com>
2017-07-19 19:44:15 +0100

    Setup bootstrap framework

---
+++ wp-content/themes/wordpress-bootstrap/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Wordpress-Bootstrap</title>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+
+
+</body>
+</html>
