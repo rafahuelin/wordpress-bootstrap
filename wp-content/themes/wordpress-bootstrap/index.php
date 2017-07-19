2620c1583bef84a64598cafb55d293107729f1c6
Rafa Huelin <it@engisa.com>
2017-07-20 00:31:43 +0100

    Add loop with post in index.php and style it

--- wp-content/themes/wordpress-bootstrap/index.php
+++ wp-content/themes/wordpress-bootstrap/index.php
@@ -11,16 +11,49 @@
                     </div>
                     <div class="panel-body">
                         <?php if(have_posts()) : ?>
-                            <?php while(have_posts()) :
-                                the_post(); ?>
-                                <div class="row">
-                                    <div class="col-md-3">
-                                        IMAGE HERE
+                            <?php while(have_posts()) : the_post(); ?>
+                                <article class="post">
+                                    <div class="row">
+                                        <?php if(has_post_thumbnail()) : ?>
+                                        <div class="col-md-3">
+                                            <div class="post-thumbnail">
+                                                <?php the_post_thumbnail(); ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-9">
+                                            <h2><a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
+
+                                            <p class="meta">
+                                                Posted at
+                                                <?php the_time(); ?> on
+                                                <?php the_date(); ?> by
+                                                <strong><?php the_author(); ?></strong>
+                                            </p>
+                                            <div class="excerpt">
+                                                <?php echo get_the_excerpt(); ?>
+                                            </div>
+                                            <br>
+                                            <a href="<?php the_permalink(); ?>" class="btn btn-default">Read More &raquo;</a>
+                                        </div>
+
+                                        <?php else : ?>
+                                            <div class="col-md-12">
+                                                <h2><a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
+                                                <p class="meta">
+                                                    Posted at
+                                                    <?php the_time(); ?> on
+                                                    <?php the_date(); ?> by
+                                                    <strong><?php the_author(); ?></strong>
+                                                </p>
+                                                <div class="excerpt">
+                                                    <?php echo get_the_excerpt(); ?>
+                                                </div>
+                                                <br>
+                                                <a href="<?php the_permalink(); ?>" class="btn btn-default">Read More &raquo;</a>
+                                            </div>
+                                        <?php endif; ?>
                                     </div>
-                                    <div class="col-md-9">
-                                        <h2><?php echo the_title(); ?></h2>
-                                    </div>
-                                </div>
+                                </article>
                             <?php endwhile; ?>
                         <?php endif; ?>
                     </div>
