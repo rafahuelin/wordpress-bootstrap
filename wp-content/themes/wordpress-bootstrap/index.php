62dd3faf7825e39e75c6f3e58df287d5599d05a6
Rafa Huelin <it@engisa.com>
2017-07-19 19:59:55 +0100

    Add sidebar area

--- wp-content/themes/wordpress-bootstrap/index.php
+++ wp-content/themes/wordpress-bootstrap/index.php
@@ -56,7 +56,9 @@
                 </div>
             </div>
             <div class="col-md-4">
-
+                <?php if(is_active_sidebar('sidebar')) : ?>
+                    <?php dynamic_sidebar('sidebar'); ?>
+                <?php endif; ?>
             </div>
         </div>
     </div>
