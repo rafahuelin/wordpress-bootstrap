867c505bb900f5546a02bac72912858f301ce8da
Rafa Huelin <it@engisa.com>
2017-07-19 20:12:53 +0100

    Place header and footer in their own files

---
+++ wp-content/themes/wordpress-bootstrap/footer.php
@@ -0,0 +1,13 @@
+    <div class="container">
+        <hr>
+        <footer>
+            <p>&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?></p>
+        </footer>
+    </div>
+
+    <script src="<?php bloginfo('template_directory'); ?>/js/jquery-3.2.1.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.js"></script>
+
+
+</body>
+</html>
--- wp-content/themes/wordpress-bootstrap/index.php
+++ wp-content/themes/wordpress-bootstrap/index.php
@@ -1,33 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Wordpress-Bootstrap</title>
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-</head>
-<body>
-
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <form class="navbar-form navbar-left">
-
-                </form>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-    </nav>
+<?php get_header(); ?>
 
     <div class="container index">
         <div class="row">
@@ -63,16 +34,4 @@
         </div>
     </div>
 
-    <div class="container">
-        <hr>
-        <footer>
-            <p>&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?></p>
-        </footer>
-    </div>
-
-    <script src="<?php bloginfo('template_directory'); ?>/js/jquery-3.2.1.min.js"></script>
-    <script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.js"></script>
-
-
-</body>
-</html>
+<?php get_footer(); ?>
