fb564573cf6bbca70254b5ab7be0f011b4686f17
Rafa Huelin <it@engisa.com>
2017-07-19 19:58:16 +0100

    Create show posts area

--- wp-content/themes/wordpress-bootstrap/index.php
+++ wp-content/themes/wordpress-bootstrap/index.php
@@ -29,5 +29,37 @@
         </div><!-- /.container-fluid -->
     </nav>
 
+    <div class="container index">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            Blog Posts
+                        </h3>
+                    </div>
+                    <div class="panel-body">
+                        <?php if(have_posts()) : ?>
+                            <?php while(have_posts()) :
+                                the_post(); ?>
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        IMAGE HERE
+                                    </div>
+                                    <div class="col-md-9">
+                                        <h2><?php echo the_title(); ?></h2>
+                                    </div>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
