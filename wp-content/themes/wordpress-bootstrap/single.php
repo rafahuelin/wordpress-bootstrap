a4fb493bc9dcdfbd01a007367162afbda23b8b6a
Rafa Huelin <it@engisa.com>
2017-07-22 12:55:35 +0100

    Create single page

---
+++ wp-content/themes/wordpress-bootstrap/single.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+
+    <div class="container index">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            Blog Posts
+                        </h3>
+                    </div>
+                    <div class="panel-body">
+                        <?php if(have_posts()) : ?>
+                            <?php while(have_posts()) : the_post(); ?>
+                                <article class="post">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <h2><a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
+                                            <!-- Check For Thumbnail-->
+                                            <?php if(has_post_thumbnail()) : ?>
+                                                <!--Display Thumbnail-->
+                                                <div class="post-thumbnail">
+                                                    <?php the_post_thumbnail(); ?>
+                                                </div>
+                                            <?php endif; ?>
+
+                                            <p class="meta">
+                                                Posted at
+                                                <?php the_time(); ?> on
+                                                <?php the_date(); ?> by
+                                                <strong><?php the_author(); ?></strong>
+                                            </p>
+                                            <div class="excerpt">
+                                                <?php echo get_the_content(); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <?php if(is_active_sidebar('sidebar')) : ?>
+                    <?php dynamic_sidebar('sidebar'); ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
